64b98f538c1e226b60856ea6ab92f941701f659b
Sunny Aggrawal <sunnyagg@outlook.com>
2023-07-07 13:52:44 +0530

    adds defaults for form data and minor changes

--- src/pages/config/index.js
+++ src/pages/config/index.js
@@ -15,38 +15,38 @@ import {
 import Editor from '../../Editor';
 import locale from 'react-json-editor-ajrm/locale/en';
 
-const BLANK_DATA = {json:{},text:{}}
-export default function ConfigPage({data=[],handleAddInput,updateInputData}) {
+const BLANK_DATA = { json: {}, text: {} }
+export default function ConfigPage({ data = [], handleAddInput, updateInputData }) {
 
-  const [newConfigNamespace, setNewConfigNamespace] = useState('test');
-  const [newConfigVersion, setNewConfigVersion] = useState('test');
-  const [newConfigGateway, setNewConfigGateway] = useState('test');
-  const [newConfigAction, setNewConfigAction] = useState('test');
+  const [newConfigNamespace, setNewConfigNamespace] = useState('demoNamespace');
+  const [newConfigVersion, setNewConfigVersion] = useState('v1');
+  const [newConfigGateway, setNewConfigGateway] = useState('demoGateway');
+  const [newConfigAction, setNewConfigAction] = useState('demoAction');
   const [newInputData, setNewInputData] = useState(BLANK_DATA);
   const [selectedInput, setSelectedInput] = useState(null);
   const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
-  const [snackbarMsg,setSnackbarMsg] = useState("")
+  const [snackbarMsg, setSnackbarMsg] = useState("")
 
-  const handleInputChange = (e,field) => {
-    switch (field){
-      case "namespace":setNewConfigNamespace(e.target.value);break;
-      case "action":setNewConfigAction(e.target.value);break;
-      case "gateway":setNewConfigGateway(e.target.value);break;
-      case "version":setNewConfigVersion(e.target.value);break;
+  const handleInputChange = (e, field) => {
+    switch (field) {
+      case "namespace": setNewConfigNamespace(e.target.value); break;
+      case "action": setNewConfigAction(e.target.value); break;
+      case "gateway": setNewConfigGateway(e.target.value); break;
+      case "version": setNewConfigVersion(e.target.value); break;
     }
   };
 
-  const resetData = ()=>{
+  const resetData = () => {
     setNewConfigAction("")
     setNewConfigGateway("")
     setNewConfigNamespace("")
     setNewConfigVersion("")
   }
   const handleInputDataChange = (newdata) => {
-    console.log("handleinptudatachange",newdata)
-    if (selectedInput!=null){
-        data[selectedInput].data = newdata
-        return 
+    console.log("handleinptudatachange", newdata)
+    if (selectedInput != null) {
+      data[selectedInput].data = newdata
+      return
     }
     setNewInputData(newdata);
   };
@@ -60,9 +60,9 @@ export default function ConfigPage({data=[],handleAddInput,updateInputData}) {
 
     const newInput = {
       namespace: newConfigNamespace.trim(),
-      action:newConfigAction.trim(),
-      gateway:newConfigGateway.trim(),
-      version:newConfigVersion.trim(),
+      action: newConfigAction.trim(),
+      gateway: newConfigGateway.trim(),
+      version: newConfigVersion.trim(),
       data: newInputData,
     };
 
@@ -73,11 +73,11 @@ export default function ConfigPage({data=[],handleAddInput,updateInputData}) {
     setIsSnackbarOpen(true);
   };
 
-  const handleOnSaveChanges = ()=>{
-      updateInputData(data)
-      setSelectedInput(null)
-      setSnackbarMsg("Config Updated Successfully")
-      setIsSnackbarOpen(true);
+  const handleOnSaveChanges = () => {
+    updateInputData(data)
+    setSelectedInput(null)
+    setSnackbarMsg("Config Updated Successfully")
+    setIsSnackbarOpen(true);
   }
   const handleInputClick = (input) => {
     setSelectedInput(input);
@@ -105,7 +105,7 @@ export default function ConfigPage({data=[],handleAddInput,updateInputData}) {
                 </React.Fragment>
               ))}
             </List>
-            <Button variant="outlined" color="primary" fullWidth onClick={()=>setSelectedInput(null)}>
+            <Button variant="outlined" color="primary" fullWidth onClick={() => setSelectedInput(null)}>
               Add New Config
             </Button>
           </Paper>
@@ -113,64 +113,64 @@ export default function ConfigPage({data=[],handleAddInput,updateInputData}) {
         <Grid item xs={8}>
           <Paper sx={{ height: '100%', padding: 2 }}>
             <Typography variant="h5" gutterBottom>
-              {selectedInput !=null? 'Edit Config' : 'Create New Config'}
+              {selectedInput != null ? 'Edit Config' : 'Create New Config'}
             </Typography>
             <Box sx={{ mt: 2 }}>
               <TextField
-                sx={{mt:1,mb:1}}
+                sx={{ mt: 1, mb: 1 }}
                 label="Namespace"
                 variant="outlined"
-                value={selectedInput!=null ? data[selectedInput].namespace: newConfigNamespace}
-                onChange={(e)=>handleInputChange(e,"namespace")}
-                fullWidth
-              />
-             <TextField
-                sx={{mt:1,mb:1}}
-                label="Gateway"
-                variant="outlined"
-                value={selectedInput!=null ? data[selectedInput].gateway: newConfigGateway}
-                onChange={(e)=>handleInputChange(e,"gateway")}
+                value={selectedInput != null ? data[selectedInput].namespace : newConfigNamespace}
+                onChange={(e) => handleInputChange(e, "namespace")}
                 fullWidth
               />
               <TextField
-                sx={{mt:1,mb:1}}
-                label="Version"
+                sx={{ mt: 1, mb: 1 }}
+                label="Gateway"
                 variant="outlined"
-                value={selectedInput!=null ? data[selectedInput].version: newConfigVersion}
-                onChange={(e)=>handleInputChange(e,"version")}
+                value={selectedInput != null ? data[selectedInput].gateway : newConfigGateway}
+                onChange={(e) => handleInputChange(e, "gateway")}
                 fullWidth
               />
-             
-                <TextField
-                sx={{mt:1,mb:1}}
+              <TextField
+                sx={{ mt: 1, mb: 1 }}
+                label="Version"
+                variant="outlined"
+                value={selectedInput != null ? data[selectedInput].version : newConfigVersion}
+                onChange={(e) => handleInputChange(e, "version")}
+                fullWidth
+              />
+
+              <TextField
+                sx={{ mt: 1, mb: 1 }}
                 label="Action"
                 variant="outlined"
-                value={selectedInput!=null ? data[selectedInput].action: newConfigAction}
-                onChange={(e)=>handleInputChange(e,"action")}
+                value={selectedInput != null ? data[selectedInput].action : newConfigAction}
+                onChange={(e) => handleInputChange(e, "action")}
                 fullWidth
               />
             </Box>
-            {console.log("data in configs",data,data[selectedInput],selectedInput)}
+            {console.log("data in configs", data, data[selectedInput], selectedInput)}
             <Box sx={{ mt: 2 }}>
               <Editor
-                data={selectedInput!=null ? data[selectedInput].data : newInputData}
+                data={selectedInput != null ? data[selectedInput].data : newInputData}
                 onChange={handleInputDataChange}
                 locale={locale}
                 height="300px"
               />
             </Box>
             <Box sx={{ mt: 2 }}>
-              <Button variant="contained" onClick={selectedInput!=null ? handleOnSaveChanges :handleOnSave} sx={{m:1}}>
-                {selectedInput!=null ? 'Save Changes' : 'Save'}
+              <Button variant="contained" onClick={selectedInput != null ? handleOnSaveChanges : handleOnSave} sx={{ m: 1 }}>
+                {selectedInput != null ? 'Save Changes' : 'Save'}
               </Button>
               <Button
                 variant="outlined"
                 onClick={
                   resetData
                 }
-                sx={{m:1}}
+                sx={{ m: 1 }}
               >
-                {selectedInput!=null ? 'Cancel' : 'Reset'}
+                {selectedInput != null ? 'Cancel' : 'Reset'}
               </Button>
             </Box>
           </Paper>
